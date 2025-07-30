e641c40bfbdc71a0b14e9d688abad26f1d3fb23d
Sreevardhini11 <167068726+Sreevardhini11@users.noreply.github.com>
2025-07-30 19:26:51 +0530

    Create Find largest of two numbers.java

---
+++ Find largest of two numbers.java
@@ -0,0 +1,7 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int a = 25, b = 40;
+        int max = (a > b) ? a : b;
+        System.out.println("Largest: " + max);
+    }
+}
