486e53d8244fc206f0d36596cf57011e3fce095e
Sreevardhini11 <167068726+Sreevardhini11@users.noreply.github.com>
2025-07-30 14:57:42 +0530

    Create logical_operations.java

---
+++ logical_operations.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean x = true, y = false;
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+    }
+}
